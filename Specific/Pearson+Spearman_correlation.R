b05faf21f88be509638df609e1bade725bcbae3a
KathiWeinberger <49943548+KathiWeinberger@users.noreply.github.com>
2019-06-06 16:01:27 +0200

    renamed

--- Specific/Pearson+Spearman_correlation.R
+++ Specific/Pearson+Spearman_correlation.R
@@ -2,13 +2,16 @@
 wd = dirname(rstudioapi::getSourceEditorContext()$path)
 Cellline_Annotation = read.delim(paste0(wd, "/Data/cellline_annotation.tsv"), header = TRUE, sep = "\t", stringsAsFactors = TRUE)
 
-# Pearson correlation+plot
+# Pearson+ Spearman correlation+plot
 
 cor(Cellline_Annotation$Doubling_Time, Cellline_Annotation$Inoculation_Density, method = "pearson")
 # 0,3209821
 
-plot(Cellline_Annotation$Doubling_Time, Cellline_Annotation$Inoculation_Density, pch= 16, col= "blue", main = "Pearson correlation between Doubling Time and Inoculation Density", xlab = "Doubling Time", ylab = "Inoculation Density")
+# Spearman correlation
+cor(Cellline_Annotation$Doubling_Time, Cellline_Annotation$Inoculation_Density, method = "spearman")
+# 0.5674381
 
+plot(Cellline_Annotation$Doubling_Time, Cellline_Annotation$Inoculation_Density, pch= 16, col= "blue", main = "Pearson correlation between Doubling Time and Inoculation Density", xlab = "Doubling Time", ylab = "Inoculation Density")
 lm(Cellline_Annotation$Inoculation_Density~ Cellline_Annotation$Doubling_Time)
 
 #   Call:
@@ -18,8 +21,5 @@ lm(Cellline_Annotation$Inoculation_Density~ Cellline_Annotation$Doubling_Time)
 #   (Intercept)  Cellline_Annotation$Doubling_Time  
 #       9110.9                              169.4   
 
-abline(9110.9, 169.4, col= "red", lwd =2)
-
-# better:
 
 abline(lm(Cellline_Annotation$Inoculation_Density ~ Cellline_Annotation$Doubling_Time), col = "red", lwd = 2)
