aa7cadf61464532d4cf4fd6dc53d825b7a211da2
TobiasKrumpf <TobiasKrumpf@users.noreply.github.com>
2019-07-13 21:59:22 +0200

    Creating HTML documents

--- lapatinib.R
+++
@@ -1,6 +0,0 @@
-# reducing Metadata to Lapatinib
-Metadata_Lapatinib_treated= Metadata[421:474, ]
-Metadata_Lapatinib_untreated= Metadata[1240:1293, ]
-summary(NegLogGI50[9, ])
-mycol <- c("blue", "red", "green", "yellow", "violet", "lightblue", "dark violet","dark green","orange")
-pie(table(Metadata_Lapatinib_treated [, "tissue"]), main = "Tissue types Lapatinib", radius = 1, cex = 1.5, col = mycol)
