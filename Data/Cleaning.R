62f9ef8783a8e861b287c73bc54e54f3629b8d09
KathiWeinberger <49943548+KathiWeinberger@users.noreply.github.com>
2019-07-21 10:10:17 +0200

    Cleaning+Final

---
+++ Data/Cleaning.R
@@ -0,0 +1,15 @@
+NCI_TPW_gep_treated <- readRDS("C:/Users/tobia/Desktop/R-Codes/Daten/NCI_TPW_gep_treated.rds")
+dat = NCI_TPW_gep_treated[, 421:474]
+LapatimibTreat <- data.frame(dat)
+rm(dat)
+NCI_TPW_gep_untreated <- readRDS("C:/Users/tobia/Desktop/R-Codes/Daten/NCI_TPW_gep_untreated.rds")
+dat = NCI_TPW_gep_untreated[, 421:474]
+LapatimibUnTreat <- data.frame(dat)
+rm(dat)
+Metadata <- read.delim("C:/Users/tobia/Desktop/R-Codes/Daten/NCI_TPW_metadata.tsv", header = TRUE, sep = "\t", stringsAsFactors = TRUE)
+Cellline_Annotation <- read.delim("C:/Users/tobia/Desktop/R-Codes/Daten/cellline_annotation.tsv", header = TRUE, sep = "\t", stringsAsFactors = TRUE)
+Drug_Annotation <- read.delim("C:/Users/tobia/Desktop/R-Codes/Daten/drug_annotation.tsv", header = TRUE, sep = "\t", stringsAsFactors = TRUE)
+CCLE_mutations <- readRDS("C:/Users/tobia/Desktop/R-Codes/Daten/CCLE_mutations.rds")
+CCLE_copynumber <- readRDS("C:/Users/tobia/Desktop/R-Codes/Daten/CCLE_copynumber.rds")
+CCLE_basalexpression <- readRDS("C:/Users/tobia/Desktop/R-Codes/Daten/CCLE_basalexpression")
+NegLogGI50 <- readRDS("C:/Users/tobia/Desktop/R-Codes/Daten/NegLogGI50.rds")
